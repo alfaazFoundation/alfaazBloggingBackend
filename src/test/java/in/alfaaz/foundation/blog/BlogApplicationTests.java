e7b59c4e9f709807ff607d8b06acaa5929c1213b
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2021-05-25 13:57:06 +0530

    Initial commit

---
+++ src/test/java/in/alfaaz/foundation/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package in.alfaaz.foundation.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
