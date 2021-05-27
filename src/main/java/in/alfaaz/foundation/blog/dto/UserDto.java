f66620b041f666acc0ef89b163e36d458f8af5d2
divyani1997 <divyanidevadiya@gmail.com>
2021-05-27 17:19:14 +0530

    user entity changes

--- src/main/java/in/alfaaz/foundation/blog/dto/UserDto.java
+++ src/main/java/in/alfaaz/foundation/blog/dto/UserDto.java
@@ -10,7 +10,11 @@ import lombok.*;
 public class UserDto {
     private Long id;
 
-    private String name;
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
 
     private String username;
 
--- src/main/java/in/alfaaz/foundation/blog/entity/UserEntity.java
+++ src/main/java/in/alfaaz/foundation/blog/entity/UserEntity.java
@@ -20,14 +20,20 @@ public class UserEntity {
     @Column(name = "ID")
     private Long id;
 
-    @Column(name = "NAME")
-    private String name;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
 
     @Column(name = "USERNAME")
     private String username;
 
     @Column(name = "PASSWORD")
     private String password;
+
+    @Column(name = "EMAIL")
+    private String email;
 
     @ManyToOne
     private RoleEntity role;
