dbe498352806e6f884a1cbf0715e6621f82bfa3d
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2021-06-09 00:10:55 +0530

    Add modifying and transactional annotations to update admin settings API

--- src/main/java/in/alfaaz/foundation/blog/repository/UserRepository.java
+++ src/main/java/in/alfaaz/foundation/blog/repository/UserRepository.java
@@ -2,8 +2,10 @@ package in.alfaaz.foundation.blog.repository;
 
 import in.alfaaz.foundation.blog.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +20,8 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     public UserEntity findById(UUID id);
 
+    @Transactional
+    @Modifying
     @Query("update UserEntity u set u.firstName = :firstName,u.lastName=:lastName,u.facebook=:facebook,u.instagram=:instagram,u.youtube=:youtube,u.twitter=:twitter WHERE u.email = :username")
     void updateAdminSettings(String username, String firstName,String lastName, String facebook, String instagram, String youtube, String twitter);
 }
