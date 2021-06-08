85955c3e6df12a36a43bcd5663743b1631c71eb6
divyani1997 <divyanidevadiya@gmail.com>
2021-06-08 23:07:36 +0530

    add blog changes

--- src/main/java/in/alfaaz/foundation/blog/dto/BlogDto.java
+++ src/main/java/in/alfaaz/foundation/blog/dto/BlogDto.java
@@ -1,5 +1,6 @@
 package in.alfaaz.foundation.blog.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import in.alfaaz.foundation.blog.enums.BlogStatus;
 import lombok.*;
 
@@ -11,17 +12,21 @@ import java.sql.Date;
 @Setter
 @ToString
 public class BlogDto {
+    @JsonIgnore
     private Long id;
 
     private String title;
 
     private String content;
 
+    @JsonIgnore
     private Date publishedOn;
 
+    @JsonIgnore
     private String publishedBy;
 
     private BlogStatus blogStatus;
 
+    @JsonIgnore
     private Date lastUpdated;
 }
--- src/main/java/in/alfaaz/foundation/blog/services/BlogDataService.java
+++ src/main/java/in/alfaaz/foundation/blog/services/BlogDataService.java
@@ -6,6 +6,8 @@ import in.alfaaz.foundation.blog.entity.UserEntity;
 import in.alfaaz.foundation.blog.repository.BlogRepository;
 import in.alfaaz.foundation.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,11 +27,14 @@ public class BlogDataService {
     }
 
     public long addOne(BlogDto blogDto){
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserEntity userEntity = userRepository.findByEmail(userDetails.getUsername());
         BlogEntity blogEntity = new BlogEntity();
         blogEntity.setTitle(blogDto.getTitle());
         blogEntity.setContent(blogDto.getContent());
-        blogEntity.setPublishedOn(blogDto.getPublishedOn());
-        blogEntity.setPublishedBy(blogDto.getPublishedBy());
+        String publishedBy = userEntity.getFirstName() + " " + userEntity.getLastName();
+        blogEntity.setPublishedBy(publishedBy);
+        blogEntity.setUser(userEntity);
 
         return blogRepository.save(blogEntity).getId();
     }
